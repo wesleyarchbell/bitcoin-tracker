86167b1d72e52caa2e3d4e45d87c16b1ef690278
Wesley Archbell <wesleyarchbell@gmail.com>
2020-02-06 21:40:49 +1000

    initial

---
+++ src/main/java/com/wesley/bitcointracker/BitcoinTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.wesley.bitcointracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitcoinTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitcoinTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wesley/bitcointracker/BitcoinTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wesley.bitcointracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitcoinTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
